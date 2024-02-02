1abb593952f590d3f6692f1f6389a37d4375378a
deminok <dominik.bluhm@gmail.com>
2024-02-02 10:19:38 +0100

    work in progress

--- Simulation/Formalization.R
+++ Simulation/Formalization.R
@@ -5,34 +5,42 @@
 
 #' Compute the updated expected anxiety
 #'
-#' @param momAnx The momentary anxiety, on a scale from 0 to 1
-#' @param prevAnx The previous expected anxiety, on a scale from 0 to 1
+#' @param momentary_anxiety The momentary anxiety, on a scale from 0 to 1
+#' @param previous_expected_anxiety The previous expected anxiety, on a scale from 0 to 1
 #' @param alpha A factor that shifts the weight between the momentary anxiety (alpha=1) and the previous expected anxiety (alpha=0) when the updated expected anxiety is computed.
-expAnx <- function(momAnx, prevAnx, alpha=0.5) {
-  momAnx*alpha + prevAnx*(1-alpha)
+expected_anxiety <- function(momentary_anxiety, previous_expected_anxiety, alpha=0.5) {
+  momentary_anxiety*alpha + previous_expected_anxiety*(1-alpha)
 }
 
 
-
-momAnx <- function(affToneIIC) {
-  0.05*exp(-2*affToneIIC)
+#' Compute the momentary anxiety
+#'
+#' @param affective_tone_iic The momentary anxiety, on a scale from 0 to 1
+momentary_anxiety <- function(affective_tone_iic) {
+  0.05*exp(-2*affective_tone_iic)
 }
 
-
-affToneIIC <- function(affToneInstruction,
-                       expAnx,
+#' Compute the updated expected anxiety
+#'
+#' @param momentary_anxiety The momentary anxiety, on a scale from 0 to 1
+#' @param previous_expected_anxiety The previous expected anxiety, on a scale from 0 to 1
+#' @param alpha A factor that shifts the weight between the momentary anxiety (alpha=1) and the previous expected anxiety (alpha=0) when the updated expected anxiety is computed.
+affective_tone_iic <- function(affective_tone_instruction,
+                       expected_anxiety,
                        beta_instruction=1,
                        beta_anx=-1,
                        beta_IA=-0.3) {
 
-    beta_instruction*affToneInstruction +
-    beta_anx*expAnx +
-    beta_IA*expAnx*affToneInstruction
+    beta_instruction*affective_tone_instruction +
+    beta_anx*expected_anxiety +
+    beta_IA*expected_anxiety*affective_tone_instruction
 }
 
-# TODO:
-appTendency <- function() {
-
+#' Compute the resulting approach tendency
+#'
+#' @param expected_anxiety The expected anxiety, on a scale from 0 to 1
+approach_tendency <- function(expected_anxiety) {
+  exp(-5 * expected_anxiety)
 }
 
 
@@ -41,19 +49,19 @@ appTendency <- function() {
 # Plots for testing
 #------------------------------------------------------------
 
-affToneIIC_values <- seq(-1, 1, by=.01)
-plot(affToneIIC_values, momAnx(affToneIIC_values), xlim=c(-1, 1), ylim=c(0, 1), type="l")
+affective_tone_iic_values <- seq(-1, 1, by=.01)
+plot(affective_tone_iic_values, momentary_anxiety(affective_tone_iic_values), xlim=c(-1, 1), ylim=c(0, 1), type="l")
 
 # test a sequence of momentary anxieties
-momAnxSeq <- c(0, 0, 0, 0.37, 0, 1, 0, 0)
+momentary_anxietySeq <- c(0, 0, 0, 0.37, 0, 1, 0, 0)
 
-expAnxRes <- c(0.3, rep(NA, length(momAnxSeq)))
+expected_anxietyRes <- c(0.3, rep(NA, length(momentary_anxietySeq)))
 
-for (i in seq_along(momAnxSeq)) {
-  expAnxRes[i+1] <- expAnx(momAnxSeq[i], expAnxRes[i], alpha=0.17)
+for (i in seq_along(momentary_anxietySeq)) {
+  expected_anxietyRes[i+1] <- expected_anxiety(momentary_anxietySeq[i], expected_anxietyRes[i], alpha=0.17)
 }
 
-plot(seq_along(expAnxRes), expAnxRes, type="b", ylim=c(0, 1))
+plot(seq_along(expected_anxietyRes), expected_anxietyRes, type="b", ylim=c(0, 1))
 
-affToneIIC(affToneInstruction=c(0, 0, 0, 1, 1, 1),
-           expAnx=c(0, .5, 1, 0, .5, 1))
+affective_tone_iic(affective_tone_instruction=c(0, 0, 0, 1, 1, 1),
+           expected_anxiety=c(0, .5, 1, 0, .5, 1))
